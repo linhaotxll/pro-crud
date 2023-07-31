95639f22a0a6a7e9bbf7a16ace742bb5ffa24de1
linhaotxl <602523587@qq.com>
2023-07-31 17:40:39 +0800

    chore: 删除样式文件

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import ElementPlus from 'element-plus'
 import { createApp } from 'vue'
 
 import 'element-plus/dist/index.css'
-import './style.css'
 import App from './App.vue'
 import { router } from './routes'
 
