8744c23159b7936f75d77bc6635086f7b3ee26fe
linhaotxl <602523587@qq.com>
2023-08-02 00:21:21 +0800

    feat: pro-form scope 遍历生成

--- core/components/ProForm/useScope.ts
+++ core/components/ProForm/useScope.ts
@@ -1,64 +1,15 @@
+import { ProFormInstanceNames } from './constant'
+
 import type { ProFormInstance, ProFormScope } from './interface'
 
 export function useScope<T extends object>(getCtx: () => ProFormInstance<T>) {
-  const scope: ProFormScope<T> = {
-    getFormValues() {
-      console.log(111, getCtx())
-      return getCtx()?.getFormValues()
-    },
+  const scope = ProFormInstanceNames.reduce((prev, curr) => {
+    // @ts-ignore
+    prev[curr] = (...args: unknown[]) => getCtx()[curr](...args)
+    return prev
+  }, {} as ProFormScope<T>)
 
-    submit() {
-      return getCtx().submit()
-    },
-
-    reset(prop) {
-      return getCtx().reset(prop)
-    },
-
-    resetFields(prop) {
-      return scope.reset(prop)
-    },
-
-    setFieldValue(prop, value) {
-      return getCtx().setFieldValue(prop, value)
-    },
-
-    setFieldValues(values) {
-      return getCtx().setFieldValues(values)
-    },
-
-    setFieldValuesTransform(values) {
-      return getCtx().setFieldValuesTransform(values)
-    },
-
-    getFieldValue(prop) {
-      return getCtx().getFieldValue(prop)
-    },
-
-    removeFields(prop) {
-      return getCtx().removeFields(prop)
-    },
-
-    validate(callback) {
-      return getCtx().validate(callback)
-    },
-
-    validateField(props, callback) {
-      return getCtx().validateField(props, callback)
-    },
-
-    scrollToField(prop) {
-      return getCtx().scrollToField(prop)
-    },
-
-    clearValidate(props) {
-      return getCtx().clearValidate(props)
-    },
-
-    getFieldInstance(prop) {
-      return getCtx().getFieldInstance(prop)
-    },
-  }
+  scope.resetFields = prop => scope.reset(prop)
 
   return scope
 }
