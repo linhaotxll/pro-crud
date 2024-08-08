29b607701f09e6eddf2d35247861362ed7270368
txll <602523587@qq.com>
2024-08-09 00:02:06 +0800

    feat: steps-form测试

--- core/components/StepsForm/buildStepsForm.ts
+++ core/components/StepsForm/buildStepsForm.ts
@@ -1,35 +1,82 @@
-import { computed, ref, toRef, watch } from 'vue'
+import { computed, ref, toRef, toValue, watch } from 'vue'
 
-import { mergeWithTovalue, type DataObject } from '../common'
-import { buildForm, BuildFormResult } from '../ProForm'
+import { mergeWithTovalue } from '../common'
+import { buildForm } from '../ProForm'
 
+import { isFunction, isPromise } from '~/utils'
+
+import type { DataObject } from './../common/interface'
 import type {
   BuildStepsFormReturn,
   StepsFormScope,
   StepsOptions,
 } from './interface'
-import type { BuildFormOptionResult, ProFormColumnOptions } from '../ProForm'
+import type { ActionOption } from '../ProButton'
+import type {
+  BuildFormOptionResult,
+  ProFormActionGroup,
+  ProFormColumnOptions,
+} from '../ProForm'
 import type { StepsProps } from 'ant-design-vue'
 import type { ComputedRef, MaybeRefOrGetter, Ref } from 'vue'
+
+export type FunctionKeys<Form extends DataObject = DataObject> = Pick<
+  BuildFormOptionResult<Form>,
+  'beforeSubmit' | 'submitRequest' | 'validateFail' | 'successRequest'
+>
 
 export function buildStepsForm<Forms extends DataObject = DataObject>(
   options: (scope: StepsFormScope<Forms>) => BuildStepsFormReturn<Forms>
 ) {
+  // 第一页索引
+  const firstIndex = 0
+
+  // 解析好的 Steps Props
   let resolvedStepProps: ComputedRef<StepsProps>
 
   const { proFormBinding } = buildForm(scope => {
-    //
-    const { stepsProps, steps, initialValues, ...commonForm } = options(scope)
-    const commonFormKeys = Object.keys(commonForm)
+    const {
+      stepsProps,
+      steps,
+      initialValues: formsInitialValues,
+      ...commonForm
+    } = options(scope)
 
+    // 步骤列表
     const items = ref<StepsProps['items']>([])
+
+    // 步骤索引映射步骤表单列
     const columnsMap = ref<
-      Record<number, MaybeRefOrGetter<ProFormColumnOptions[]> | undefined>
+      Record<
+        number,
+        | MaybeRefOrGetter<ProFormColumnOptions<Forms[string], any, any>[]>
+        | undefined
+      >
     >({})
 
-    const current = ref(0)
+    // 步骤索引映射表单操作
+    const actionMap = ref(
+      {} as Record<number, MaybeRefOrGetter<ProFormActionGroup> | undefined>
+    )
 
-    // 解析 steps props
+    // 步骤索引映射表单名称前缀
+    const stepsPrefix = ref<Record<number, string>>({})
+
+    // 步骤索引映射表单 Props
+    const formPropsMap = ref<
+      Record<number, BuildFormOptionResult['formProps']>
+    >({})
+
+    // 步骤对象
+    const stepsRef = toRef(steps) as Ref<StepsOptions<Forms>>
+
+    //  步骤索引映射表单操作
+    const funcMap: Record<number, FunctionKeys> = {}
+
+    // 当前步骤索引
+    const current = ref(firstIndex)
+
+    // 解析 Steps Props
     resolvedStepProps = computed<StepsProps>(() =>
       mergeWithTovalue({}, stepsProps, {
         items,
@@ -40,23 +87,176 @@ export function buildStepsForm<Forms extends DataObject = DataObject>(
       })
     )
 
+    // 下一步按钮
+    const nextButton: ActionOption = {
+      text: '下一步',
+    }
+
+    // 上一步按钮
+    const previousButton: ActionOption = {
+      text: '上一步',
+      props: { onClick: handlePreviousStep },
+    }
+
+    // 完成按钮
+    const finishButton: ActionOption = {
+      text: '完成',
+      props: { type: 'primary' },
+    }
+
+    // 第一页按钮
+    const firstStepAction: ProFormActionGroup = {
+      actions: {
+        confirm: nextButton,
+      },
+    }
+
+    // 中间页按钮
+    const middleStepAction: ProFormActionGroup = {
+      actions: {
+        previous: previousButton,
+        confirm: nextButton,
+      },
+    }
+
+    // 最后一页页按钮
+    const lastStepAction: ProFormActionGroup = {
+      actions: {
+        previous: previousButton,
+        confirm: finishButton,
+      },
+    }
+
+    // 是否是最后一步
+    function isLast() {
+      return current.value === Object.keys(stepsRef.value).length
+    }
+
+    let resolvedSubmitParams: any | null = null
+
     const buildFormReturn: BuildFormOptionResult = {
-      columns: computed(() => columnsMap.value[current.value]),
+      columns: computed(() => {
+        return columnsMap.value[current.value] as any
+      }),
+      action: computed(() => {
+        let defaultAction: ProFormActionGroup = middleStepAction
+        const currentStep = current.value
+        if (currentStep === firstIndex) {
+          defaultAction = firstStepAction
+        } else if (currentStep === Object.keys(stepsRef.value).length - 1) {
+          defaultAction = lastStepAction
+        }
+        return mergeWithTovalue(
+          {},
+          defaultAction,
+          actionMap.value[current.value]
+        )
+      }),
+
+      initialValues: formsInitialValues,
+
+      submitRequest(values) {
+        return funcMap[current.value].submitRequest?.(values) ?? true
+      },
+      successRequest(values) {
+        const result = funcMap[current.value].successRequest?.(values) ?? values
+        if (isPromise(result)) {
+          return result.then(a)
+        } else {
+          return a()
+        }
+
+        function a() {
+          if (!isLast()) {
+            ;(resolvedSubmitParams ||= {})[stepsPrefix.value[current.value]] =
+              result
+
+            handleNextStep()
+            if (isLast()) {
+              return scope.submit()
+            }
+          } else {
+            current.value = 0
+          }
+        }
+      },
+      validateFail(e) {
+        return funcMap[current.value].validateFail?.(e)
+      },
+      beforeSubmit() {
+        const prefix = stepsPrefix.value[current.value]
+        const stepForm = prefix
+          ? scope.getFieldValue(prefix)
+          : resolvedSubmitParams
+        return funcMap[current.value].beforeSubmit?.(stepForm) ?? stepForm
+      },
+
+      toast: false,
+
+      name: computed(() => {
+        return stepsPrefix.value[current.value]
+      }),
+
+      formProps: computed(() => {
+        const { rules, ...props } = toValue(
+          formPropsMap.value[current.value] || {}
+        )
+
+        return mergeWithTovalue({}, toValue(commonForm.formProps), props, {
+          rules: rules
+            ? mergeWithTovalue(
+                {},
+                {
+                  [stepsPrefix.value[current.value]]: rules,
+                }
+              )
+            : undefined,
+        })
+      }),
+    }
+
+    function handleNextStep() {
+      if (current.value < Object.keys(stepsRef.value).length) {
+        current.value++
+      }
+    }
+
+    function handlePreviousStep() {
+      if (current.value > 0) {
+        current.value = current.value - (isLast() ? 2 : 1)
+      }
     }
 
     watch(
-      toRef(steps) as Ref<StepsOptions<Forms>>,
+      stepsRef,
       value => {
         const tempItems: StepsProps['items'] = []
         const tempColumnsMap: Record<
           number,
-          MaybeRefOrGetter<ProFormColumnOptions[]> | undefined
+          | MaybeRefOrGetter<ProFormColumnOptions<Forms[string], any, any>[]>
+          | undefined
+        > = {}
+        const tempAction: Record<
+          number,
+          MaybeRefOrGetter<ProFormActionGroup> | undefined
         > = {}
 
-        Object.keys(value)
+        const stepsKeys = Object.keys(value)
+
+        const tempStepsPrefix: Record<number, string> = {}
+        let tempFormProps: Record<
+          number,
+          BuildFormOptionResult['formProps']
+        > | null = null
+
+        let initialFormValues: Record<string, any> | null = null
+
+        stepsKeys
           .sort((a, b) => (value[a].order ?? 0) - (value[b].order ?? 0))
           .forEach((key, index) => {
             const options = value[key]
+
+            ;(initialFormValues ||= {})[key] = {}
 
             const {
               description,
@@ -67,7 +267,9 @@ export function buildStepsForm<Forms extends DataObject = DataObject>(
               subTitle,
               onClick,
               columns,
-              initialValues,
+              action,
+              // initialValues,
+              formProps,
               ...stepForm
             } = options
 
@@ -82,20 +284,41 @@ export function buildStepsForm<Forms extends DataObject = DataObject>(
             })
 
             tempColumnsMap[index] = columns
+            tempAction[index] = action
+            tempStepsPrefix[index] = key
+            ;(tempFormProps ||= {})[index] = formProps
 
             const stepFormKeys = Object.keys(stepForm)
-            const resolvedFormKeys = new Set(
-              stepFormKeys.concat(commonFormKeys)
-            )
-            for (const key of resolvedFormKeys) {
-              buildFormReturn[key] = computed(() =>
-                mergeWithTovalue({}, commonForm[key], stepForm[key])
-              )
+
+            for (const key of stepFormKeys) {
+              const formValue = (stepForm as any)[key]
+              if (isFunction(formValue)) {
+                ;(funcMap[index] ||= {})[key as keyof FunctionKeys] = formValue
+              } else {
+                buildFormReturn[key as keyof typeof buildFormReturn] = computed(
+                  () => {
+                    return mergeWithTovalue(
+                      {},
+                      commonForm[key as keyof typeof commonForm],
+                      (value as any)[stepsKeys[current.value]][key]
+                    )
+                  }
+                ) as any
+              }
             }
           })
 
+        funcMap[stepsKeys.length] = {
+          submitRequest: commonForm.submitRequest,
+          beforeSubmit: commonForm.beforeSubmit,
+          validateFail: commonForm.validateFail,
+          successRequest: commonForm.successRequest,
+        }
+
         items.value = tempItems
         columnsMap.value = tempColumnsMap
+        stepsPrefix.value = tempStepsPrefix
+        formPropsMap.value = tempFormProps!
       },
       { immediate: true }
     )
---
+++ core/components/StepsForm/test/build.steps.form.spec.ts
@@ -0,0 +1,201 @@
+import { mount } from '@vue/test-utils'
+import antdv, { Button, Input, Select, Steps } from 'ant-design-vue'
+import { describe, expect, test, vi } from 'vitest'
+import { defineComponent, h, nextTick } from 'vue'
+
+import { StepsForm, buildStepsForm } from '..'
+import { ProComponents } from '../../..'
+
+import type { StepsFormScope } from '..'
+
+const sleep = (time: number) => new Promise(r => setTimeout(r, time))
+
+describe('StepsForm', () => {
+  test('basic', async () => {
+    let scope!: StepsFormScope
+    const validateFail = vi.fn(() => {
+      //
+    })
+    const infoBeforeSubmit = vi.fn((values: any) => ({
+      ...values,
+      a: 1,
+    }))
+    const infoSubmitRequest = vi.fn(() => {
+      return true
+    })
+
+    const extendsBeforeSubmit = vi.fn((values: any) => ({
+      ...values,
+      b: 2,
+    }))
+    const extendsSubmitRequest = vi.fn(() => {
+      return true
+    })
+
+    const topBeforeSubmit = vi.fn(values => {
+      return {
+        ...values,
+        c: 3,
+      }
+    })
+    const topSubmitRequest = vi.fn(() => {
+      return true
+    })
+
+    const App = defineComponent({
+      name: 'App',
+      setup() {
+        const { stepsFormBinding } = buildStepsForm(s => {
+          scope = s
+
+          return {
+            steps: {
+              info: {
+                title: '基本信息',
+                columns: [
+                  {
+                    label: '姓名',
+                    name: 'name',
+                  },
+                ],
+                validateFail,
+                formProps: {
+                  rules: {
+                    name: { required: true, message: '请填写姓名' },
+                  },
+                },
+                col: { span: 8 },
+                beforeSubmit: infoBeforeSubmit,
+                submitRequest: infoSubmitRequest,
+              },
+
+              extends: {
+                title: '扩展信息',
+                columns: [
+                  {
+                    label: '学校',
+                    name: 'school',
+                    type: 'select',
+                    dict: {
+                      data: [{ label: '清华大学', value: 'qh' }],
+                    },
+                  },
+                ],
+                beforeSubmit: extendsBeforeSubmit,
+                submitRequest: extendsSubmitRequest,
+              },
+            },
+
+            beforeSubmit: topBeforeSubmit,
+            submitRequest: topSubmitRequest,
+          }
+        })
+        return () => {
+          return h(StepsForm, stepsFormBinding)
+        }
+      },
+    })
+
+    const wrapper = mount(App, {
+      global: {
+        plugins: [antdv, ProComponents],
+      },
+    })
+
+    expect(wrapper.findAllComponents(Steps).length).toBe(1)
+
+    expect(wrapper.findAllComponents(Input).length).toBe(1)
+
+    expect(wrapper.findComponent(Steps).vm.$props.items).toMatchInlineSnapshot(`
+      [
+        {
+          "description": undefined,
+          "disabled": undefined,
+          "icon": undefined,
+          "onClick": undefined,
+          "status": undefined,
+          "subTitle": undefined,
+          "title": "基本信息",
+        },
+        {
+          "description": undefined,
+          "disabled": undefined,
+          "icon": undefined,
+          "onClick": undefined,
+          "status": undefined,
+          "subTitle": undefined,
+          "title": "扩展信息",
+        },
+      ]
+    `)
+
+    expect(wrapper.findAllComponents(Button).length).toBe(1)
+    expect(wrapper.findAllComponents(Button)[0].text()).toBe('下一步')
+
+    await wrapper.findAllComponents(Button)[0].vm.$emit('click')
+    await sleep(0)
+
+    expect(validateFail).toHaveBeenCalledTimes(1)
+    expect((validateFail.mock.calls[0] as any)[0]).toMatchInlineSnapshot(`
+      {
+        "errorFields": [
+          {
+            "errors": [
+              "请填写姓名",
+            ],
+            "name": [
+              "info",
+              "name",
+            ],
+            "warnings": [],
+          },
+        ],
+        "outOfDate": false,
+        "values": {
+          "info": {
+            "name": undefined,
+          },
+        },
+      }
+    `)
+
+    // TODO:
+    scope.setFieldValue(['info', 'name'], 'IconMan')
+
+    await nextTick()
+
+    expect(wrapper.findComponent(Input).vm.$props.value).toBe('IconMan')
+    expect(wrapper.findComponent(Input).find('input').element.value).toBe(
+      'IconMan'
+    )
+
+    await wrapper.findAllComponents(Button)[0].find('button').trigger('click')
+    await sleep(0)
+
+    expect(validateFail).toHaveBeenCalledTimes(1)
+    expect(infoBeforeSubmit).toHaveReturnedTimes(1)
+    expect(infoBeforeSubmit).toHaveBeenCalledWith({ name: 'IconMan' })
+    expect(infoSubmitRequest).toHaveBeenCalledTimes(1)
+    expect(infoSubmitRequest).toHaveBeenCalledWith({ name: 'IconMan', a: 1 })
+
+    expect(wrapper.findAllComponents(Input).length).toBe(0)
+    expect(wrapper.findAllComponents(Select).length).toBe(1)
+
+    expect(wrapper.findAllComponents(Button).length).toBe(2)
+    expect(wrapper.findAllComponents(Button)[0].text()).toBe('完 成')
+    expect(wrapper.findAllComponents(Button)[1].text()).toBe('上一步')
+
+    scope.setFieldValue(['extends', 'school'], 'qh')
+    await nextTick()
+
+    wrapper.findAllComponents(Button)[0].vm.$emit('click')
+    await sleep(0)
+
+    expect(extendsBeforeSubmit).toHaveBeenCalledTimes(1)
+    expect(extendsBeforeSubmit).toHaveBeenCalledWith({ school: 'qh' })
+    expect(extendsSubmitRequest).toHaveBeenCalledTimes(1)
+    expect(extendsSubmitRequest).toHaveBeenCalledWith({ school: 'qh', b: 2 })
+
+    expect(topBeforeSubmit).toHaveBeenCalledTimes(1)
+  })
+})
